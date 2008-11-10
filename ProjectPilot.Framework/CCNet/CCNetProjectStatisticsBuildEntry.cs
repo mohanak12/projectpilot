6b04aa13ae216000d4a3bf512bc922ddd548d3f2
gregor.kocevar <gregor.kocevar@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-10 07:06:00 +0000

    Added xml parses for ccnet statistics.

--- ProjectPilot.Framework/CCNet/CCNetProjectStatisticsBuildEntry.cs
+++ ProjectPilot.Framework/CCNet/CCNetProjectStatisticsBuildEntry.cs
@@ -5,11 +5,37 @@ namespace ProjectPilot.Framework.CCNet
 {
     public class CCNetProjectStatisticsBuildEntry
     {
+        #region Private members
+
         [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
         private string buildLabel;
         [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
         private string buildStatus;
         [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
         private Dictionary<string, string> parameters = new Dictionary<string, string>();
+
+        #endregion
+
+        #region Public properties
+
+        public string BuildLabel
+        {
+            get { return buildLabel; }
+            set { buildLabel = value; }
+        }
+
+        public string BuildStatus
+        {
+            get { return buildStatus; }
+            set { buildStatus = value; }
+        }
+
+        public Dictionary<string, string> Parameters
+        {
+            get { return parameters; }
+            set { parameters = value; }
+        }
+
+        #endregion
     }
 }
--- ProjectPilot.Framework/CCNet/CCNetProjectStatisticsData.cs
+++ ProjectPilot.Framework/CCNet/CCNetProjectStatisticsData.cs
@@ -1,14 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
-using System.Text;
 
 namespace ProjectPilot.Framework.CCNet
 {
     public class CCNetProjectStatisticsData
     {
+        #region Private member
+
         [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
         private List<CCNetProjectStatisticsBuildEntry> builds = new List<CCNetProjectStatisticsBuildEntry>();
+
+        #endregion
+
+        #region Public property
+
+        public List<CCNetProjectStatisticsBuildEntry> Builds
+        {
+            get { return builds; }
+            set { builds = value; }
+        }
+
+        #endregion
     }
 }
