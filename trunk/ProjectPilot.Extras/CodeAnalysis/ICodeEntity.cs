1d077d65e2a0c2f2134053ef476383be9c7705b1
igor.brejc <igor.brejc@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-14 17:50:43 +0000

    Continued implementing code analysis functionality.

--- trunk/ProjectPilot.Extras/CodeAnalysis/ICodeEntity.cs
+++ trunk/ProjectPilot.Extras/CodeAnalysis/ICodeEntity.cs
@@ -1,9 +1,21 @@
+using System.Collections.Generic;
+
 namespace ProjectPilot.Extras.CodeAnalysis
 {
+    public enum CodeEntityAnalysisState
+    {
+        NotAnalyzed,
+        InAnalysis,
+        Analyzed
+    }
+
     public interface ICodeEntity
     {
+        CodeEntityAnalysisState AnalysisState { get; set; }
+        ICollection<ICodeEntity> Ancestors {get;}
         string EntityShortName { get; }
         string EntityFullName { get; }
+        int UniqueId { get; set; }
 
         void AddAncestor(ICodeEntity ancestor);
     }
---
+++ trunk/ProjectPilot.Tests/Extras/CodeAnalysisTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MbUnit.Framework;
+using ProjectPilot.Extras.CodeAnalysis;
+using ProjectPilot.Framework;
+
+namespace ProjectPilot.Tests.Extras
+{
+    [TestFixture]
+    public class CodeAnalysisTests
+    {
+        [Test]
+        public void Test()
+        {
+            AssemblyCodeAnalyzer analyzer = new AssemblyCodeAnalyzer(typeof(Project).Assembly);
+            CodeAnalysisGraph graph = analyzer.AnalyzeCode();
+
+            using (Stream stream = File.Open("test.dot", FileMode.Create))
+            {
+                UmlClassDiagramGenerator generator = new UmlClassDiagramGenerator();
+                generator.GenerateDiagram(graph, stream);
+            }
+        }
+    }
+}
