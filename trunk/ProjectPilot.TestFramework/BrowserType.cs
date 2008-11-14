f02f2566e84b6d5b0df841c2ba3f99ed5313c352
andrej.jeznik <andrej.jeznik@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-14 10:36:39 +0000

    Added SeleniumTesterBase class

---
+++ trunk/ProjectPilot.TestFramework/BrowserType.cs
@@ -0,0 +1,18 @@
+namespace ProjectPilot.TestFramework
+{
+    public enum BrowserType
+    {
+        /// <summary>
+        /// None
+        /// </summary>
+        None,
+        /// <summary>
+        /// Internet Explorer
+        /// </summary>
+        InternetExplorer,
+        /// <summary>
+        /// Firefox
+        /// </summary>
+        Firefox,
+    }
+}
