6ecf8f6893fac40085a5eeb5e9cdcb2d2dc67df9
andrej.jeznik <andrej.jeznik@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-27 09:14:37 +0000

    Added InputArguments.

--- Accipio.Console/ConsoleApp.cs
+++ Accipio.Console/ConsoleApp.cs
@@ -1,26 +1,9 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace Accipio.Console
 {
-    // generate business actions XML schema file
-    // input: test actions XML file
-    // output: test specifications XML schema file
-    // what has to be done:
-    //   1. validating XML with schema file (automatic)
-    //   2. parsing XML file and retrieving TestActions, parameters etc
-    //     - check that action ID's and parameter names do not have whitespaces
-    //   3. generating XSD file which contains these actions
-
-    //  Generating testing source code and documentation
-    // # Inputs: business actions XML file, business actions XML schema, test cases XML file
-    // # Outputs:
-    //   * C# code files which execute the specified tests
-    //   * Test case documentation in HTML format 
-
-    // command line: command arguments
-    // genbaxsd 
-    // gencode 
     public class ConsoleApp
     {
         public ConsoleApp(string[] args)
@@ -31,7 +14,51 @@ namespace Accipio.Console
         public void Process()
         {
             ShowBanner();
-            System.Console.WriteLine(args.Length);
+            int numberOfArguments = args.Length;
+            if (numberOfArguments < 2)
+            {
+                ShowHelp();
+                return;
+            }
+            
+            string generatorCommand = args[0];
+            InputArgument inputArgument = 
+                (InputArgument) Enum.Parse(typeof (InputArgument), generatorCommand, true);
+            switch (inputArgument)
+            {
+                case InputArgument.BusinessAction:
+                    {
+                        // generate business actions XML schema file
+                        // input: test actions XML file
+                        // output: test specifications XML schema file
+                        // what has to be done:
+                        //   1. validating XML with schema file (automatic)
+                        //   2. parsing XML file and retrieving TestActions, parameters etc
+                        //     - check that action ID's and parameter names do not have whitespaces
+                        //   3. generating XSD file which contains these actions
+                        break;
+                    }
+                case InputArgument.TestSpec:
+                    {
+                        //  Generating testing source code and documentation
+                        // # Inputs: business actions XML file, business actions XML schema, test cases XML file
+                        // # Outputs:
+                        //   * C# code files which execute the specified tests
+                        //   * Test case documentation in HTML format 
+                        
+                        break;
+                    }
+                default:
+                    {
+                        ShowHelp();
+                        break;
+                    }
+            }
+        }
+
+        private static void ShowHelp()
+        {
+            System.Console.WriteLine("Usage: Accipio.Conole [OPTIONS]");
         }
 
         private static void ShowBanner()
