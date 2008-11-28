1b00f8d8f10a4050e608c2b57f7fe14812111e22
gregor.kocevar <gregor.kocevar@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-28 10:10:00 +0000

    Added accipio helper class

---
+++ Accipio.Console/AccipioHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace Accipio.Console
+{
+    /// <summary>
+    /// AccipioHelper class contains helper methods
+    /// </summary>
+    class AccipioHelper
+    {
+        private AccipioHelper()
+        { }
+
+        /// <summary>
+        /// Gets the content from xml file.
+        /// </summary>
+        /// <param name="fileName">Xml file name to be readed as stream</param>
+        /// <returns>Content of xml file as stream</returns>
+        public static Stream GetXmlFileContent(string fileName)
+        {
+            string fileShema = fileName;
+            FileInfo fileInfoShema = new FileInfo(fileShema);
+            if (File.Exists(fileInfoShema.FullName))
+            {
+                if (fileInfoShema.Extension.Equals(".xml", StringComparison.OrdinalIgnoreCase))
+                {
+                    return ReadFile(fileInfoShema.FullName);
+                }
+            }
+
+            return null;
+        }
+
+        private static Stream ReadFile(string fileName)
+        {
+            Stream content;
+            using (StreamReader streamReader = new StreamReader(fileName))
+            {
+                content = streamReader.BaseStream;
+            }
+
+            return content;
+        }
+    }
+}
--- Accipio.Console/ConsoleApp.cs
+++ Accipio.Console/ConsoleApp.cs
@@ -15,83 +15,53 @@ namespace Accipio.Console
 
         public void Process()
         {
-            ShowBanner();
-            int numberOfArguments = args.Length;
-            if (numberOfArguments < 2)
+            try
             {
-                ShowHelp();
-                return;
-            }
-            
-            string generatorCommand = args[0];
-            InputArgument inputArgument = 
-                (InputArgument) Enum.Parse(typeof (InputArgument), generatorCommand, true);
-            
-            switch (inputArgument)
-            {
-                case InputArgument.BusinessAction:
-                    {
-                        // generate business actions XML schema file
-                        // input: test actions XML file
-                        // output: test specifications XML schema file
-                        // what has to be done:
-                        //   1. validating XML with schema file (automatic)
-                        //   2. parsing XML file and retrieving TestActions, parameters etc
-                        //     - check that action ID's and parameter names do not have whitespaces
-                        //   3. generating XSD file which contains these actions
-                        IGenerator generator = new BusinessActionGenerator(GetXmlFileContent(args[1]));
-                        generator.Parse(args);
-                        //if (!generator.Parse())
-                        //{
-                        //    System.Console.WriteLine("Parsing error...");
-                        //    return;
-                        //}
-                        generator.Process();
-                        break;
-                    }
-                case InputArgument.TestSpec:
-                    {
-                        //  Generating testing source code and documentation
-                        // # Inputs: business actions XML file, business actions XML schema, test cases XML file
-                        // # Outputs:
-                        //   * C# code files which execute the specified tests
-                        //   * Test case documentation in HTML format 
-                        
-                        break;
-                    }
-                default:
-                    {
-                        ShowHelp();
-                        break;
-                    }
-            }
-        }
+                ShowBanner();
 
-        private static string GetXmlFileContent(string fileName)
-        {
-            string fileShema = fileName;
-            FileInfo fileInfoShema = new FileInfo(fileShema);
-            if (File.Exists(fileInfoShema.FullName))
-            {
-                if (fileInfoShema.Extension.Equals(".xml", StringComparison.OrdinalIgnoreCase))
+                string generatorCommand = args[0];
+                InputArgument inputArgument =
+                    (InputArgument) Enum.Parse(typeof (InputArgument), generatorCommand, true);
+
+                switch (inputArgument)
                 {
-                    return ReadFile(fileInfoShema.FullName);
+                    case InputArgument.BusinessAction:
+                        {
+                            // generate business actions XML schema file
+                            // input: test actions XML file
+                            // output: test specifications XML schema file
+                            // what has to be done:
+                            //   1. validating XML with schema file (automatic)
+                            //   2. parsing XML file and retrieving TestActions, parameters etc
+                            //     - check that action ID's and parameter names do not have whitespaces
+                            //   3. generating XSD file which contains these actions
+                            IGenerator generator = new BusinessActionGenerator();
+                            generator.Parse(args);
+                            generator.Process();
+
+                            break;
+                        }
+                    case InputArgument.TestSpec:
+                        {
+                            //  Generating testing source code and documentation
+                            // # Inputs: business actions XML file, business actions XML schema, test cases XML file
+                            // # Outputs:
+                            //   * C# code files which execute the specified tests
+                            //   * Test case documentation in HTML format 
+
+                            break;
+                        }
+                    default:
+                        {
+                            ShowHelp();
+                            break;
+                        }
                 }
             }
-            return String.Empty;
-        }
-
-        private static string ReadFile(string fileName)
-        {
-            string content;
-            using (StreamReader streamReader = new StreamReader(fileName))
+            catch (ApplicationException aex)
             {
-                content = streamReader.ReadToEnd();
-                streamReader.Close();
-                streamReader.Dispose();
+                System.Console.WriteLine("Parsing error... Details: {0}", aex);
             }
-            return content;
-
         }
 
         private static void ShowHelp()
