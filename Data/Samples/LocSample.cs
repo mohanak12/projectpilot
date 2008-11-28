4d04fbc1c2497b72624c24776940c54f8dcaf0f3
jure.hribar@gmail.com <jure.hribar@gmail.com@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-28 14:06:50 +0000

    Work in progress: implementing LOC metrics C# style.

---
+++ Data/Samples/LocSample.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+    
+namespace ProjectPilot.Framework.Metrics
+{
+    public class LocStats : ILocStats
+    {
+            LocStatsData returnData = new LocStatsData(sloc, cloc, eloc);
+
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                        break;
+                    else
+                    {
+                        sloc++;
+                    }
+
+                    /*Regex regex = new Regex("");*/
+                }
+            }
+            return returnData;
+        }
+
+        public LocStatsData CountLocString(string code)
+        {
+
+            if (tmpChar != '\n') sloc++; //The last line doesn't allways end with a \n but still needs to be counted
+        
+
+        }
+
+        public LocStatsData CountLocFile(string filePath)
+        {
+            int sloc = 0;
+            int cloc = 0;
+            int eloc = 0;
+
+            /*string line = "// this is not a comment!!!";*/
+
+            if (File.Exists(filePath))
+            {
+                //try
+                //{
+                stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+
+                while ((tmpChar = stream.ReadByte()) != -1)
+                {
+                    // Counting comments
+                    if (tmpChar == '\n')
+                    {
+                        sloc++;
+                        if (notEmpty == false)
+                            eloc++;
+                        else
+                            notEmpty = false;
+                    }
+
+                /*}/* /* /// 
+                catch (Exception e)
+                {////*
+                    //plkjlkjlj
+                }*/
+            }
+
+            //comment
+
+            return returnData;
+        }
+    }
+}
