ffcc3bfc270e632bb651ed68f337789a0b4add2a
andrej.jeznik <andrej.jeznik@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-28 13:50:08 +0000

    Added parser for c# code and html test specifications

--- Accipio.Console/TestSpecGenerator.cs
+++ Accipio.Console/TestSpecGenerator.cs
@@ -10,11 +10,53 @@ namespace Accipio.Console
         public void Parse(string[] args)
         {
             AccipioHelper.CheckForValidInputArguments(args);
+            FileInfo businessActionXML = new FileInfo(args[1]);
+            FileInfo businessActionXSD = new FileInfo(args[2]);
+            //TODO: possible more than one XML file with test cases.
+            FileInfo testCasesXML = new FileInfo(args[3]);
+
+            //TODO: uncoment when BusinessActions ready
+            //parse business actions
+            //XmlTestSpecsParser parser =
+            //    new XmlTestSpecsParser(AccipioHelper.GetXmlFileContent(businessActionXML.FullName));
+            //BusinessActions businessActions = parser.Parse();
+
+            //validate XML content
+            Stream streamBusinessActions = 
+                AccipioHelper.GetXmlFileContent(businessActionXML.FullName);
+            AccipioHelper.ValidateXmlDocument(streamBusinessActions, businessActionXSD.FullName);
+
+            //parse test cases
+            Stream streamTestCases =
+                AccipioHelper.GetXmlFileContent(testCasesXML.FullName);
+            XmlTestSpecsParser testSpecsParser = new XmlTestSpecsParser(streamTestCases);
+            TestSpecs testSpecs = testSpecsParser.Parse();
+            
+            //generate c# code
+            //TODO: add business actions descriptions to generated code
+            string generatedFileName = testCasesXML.FullName.Substring(0, testCasesXML.FullName.Length - 4);
+            string csharpCodeFile = generatedFileName + ".cs";
+            System.Console.WriteLine("Creating '{0}'", csharpCodeFile);
+            using (ICodeWriter writer = new FileCodeWriter(csharpCodeFile))
+            {
+                ITestCodeGenerator cSharpCode = new CSharpTestCodeGenerator(writer);
+                cSharpCode.Generate(testSpecs);
+            }
+
+            //generate html test specifications
+            //TODO: add business actions descriptions to test spec
+            string htmlCodeFile = generatedFileName + ".html";
+            System.Console.WriteLine("Creating '{0}'", htmlCodeFile);
+            using (ICodeWriter writer = new FileCodeWriter(htmlCodeFile))
+            {
+                ITestCodeGenerator htmlTestSpecs = new HtmlTestCodeGenerator(writer);
+                htmlTestSpecs.Generate(testSpecs);
+            }
         }
 
         public void Process()
         {
-            throw new System.NotImplementedException();
+            //TODO: move from parser
         }
     }
 }
