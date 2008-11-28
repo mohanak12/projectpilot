74186fc2f14f9ea35346369b02f392b4fe7ea9c2
gregor.kocevar <gregor.kocevar@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-28 12:52:48 +0000

    Added xml validation method

--- trunk/Accipio.Console/AccipioHelper.cs
+++ trunk/Accipio.Console/AccipioHelper.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace Accipio.Console
 {
@@ -32,5 +36,45 @@ namespace Accipio.Console
 
             return null;
         }
+
+        /// <summary>
+        /// Validate xml document with predefined xsd schema
+        /// </summary>
+        /// <param name="stream">Xml file to be validated</param>
+        /// <param name="xsdFileName">Path to xsd schema file</param>
+        public static void ValidateXmlDocument(Stream stream, string xsdFileName)
+        {
+            // create and set validation settings
+            XmlReaderSettings xmlReaderSettings = new XmlReaderSettings();
+            xmlReaderSettings.ValidationType = ValidationType.Schema;
+            // add schema
+            XmlSchemaSet xmlSchemaSet = new XmlSchemaSet();
+            xmlSchemaSet.Add(null, xsdFileName);
+            xmlReaderSettings.Schemas = xmlSchemaSet;
+            xmlReaderSettings.ValidationEventHandler += new ValidationEventHandler(ValidationCallBack);
+
+            // create reader
+            XmlReader xmlReader = XmlReader.Create(stream, xmlReaderSettings);
+
+            // parse file
+            while (xmlReader.Read())
+            { }
+
+            if (validationStatus.Length > 0)
+            {
+                throw new XmlException(string.Format(CultureInfo.InvariantCulture,
+                    "Xml file does not match to validation schema. Details: {0}", validationStatus));
+            }
+        }
+
+        /// <summary>
+        /// Save validation error
+        /// </summary>
+        private static void ValidationCallBack(object sender, ValidationEventArgs args)
+        {
+            validationStatus.Append(string.Format(CultureInfo.InvariantCulture, "Validation Error: {0} \n", args.Message));
+        }
+
+        private static StringBuilder validationStatus = new StringBuilder(); 
     }
 }
