92eaa5b46c2e89593c814549284f5d85ea2ba888
andrej.jeznik <andrej.jeznik@5ada41ee-a197-11dd-9b86-41075523de00>
2008-11-30 07:40:26 +0000

    Process of test files generation.

--- Accipio.Console/TestSpecGenerator.cs
+++ Accipio.Console/TestSpecGenerator.cs
@@ -1,62 +1,73 @@
+#region
+
 using System.IO;
+
+#endregion
 
 namespace Accipio.Console
 {
-    /// <summary>
-    /// Generating testing source code and documentation.
-    /// </summary>
-    public class TestSpecGenerator : IGenerator
-    {
-        public void Parse(string[] args)
-        {
-            AccipioHelper.CheckForValidInputArguments(args);
-            FileInfo businessActionXML = new FileInfo(args[1]);
-            FileInfo businessActionXSD = new FileInfo(args[2]);
-            //TODO: possible more than one XML file with test cases.
-            FileInfo testCasesXML = new FileInfo(args[3]);
+	/// <summary>
+	/// Generating testing source code and documentation.
+	/// </summary>
+	public class TestSpecGenerator : IGenerator
+	{
+		public void Parse(string[] args)
+		{
+			AccipioHelper.CheckForValidInputArguments(args);
 
-            //TODO: uncoment when BusinessActions ready
-            //parse business actions
-            //XmlTestSpecsParser parser =
-            //    new XmlTestSpecsParser(AccipioHelper.GetXmlFileContent(businessActionXML.FullName));
-            //BusinessActions businessActions = parser.Parse();
+			businessActionXML = new FileInfo(args[1]);
+			businessActionXSD = new FileInfo(args[2]);
+			//TODO: possible more than one XML file with test cases.
+			testCasesXML = new FileInfo(args[3]);
 
-            //validate XML content
-            Stream streamBusinessActions = 
-                AccipioHelper.GetXmlFileContent(businessActionXML.FullName);
-            AccipioHelper.ValidateXmlDocument(streamBusinessActions, businessActionXSD.FullName);
+			//TODO: uncoment when BusinessActions ready
+			//parse business actions
+			//XmlTestSpecsParser parser =
+			//    new XmlTestSpecsParser(AccipioHelper.GetXmlFileContent(businessActionXML.FullName));
+			//BusinessActions businessActions = parser.Parse();
 
-            //parse test cases
-            Stream streamTestCases =
-                AccipioHelper.GetXmlFileContent(testCasesXML.FullName);
-            XmlTestSpecsParser testSpecsParser = new XmlTestSpecsParser(streamTestCases);
-            TestSpecs testSpecs = testSpecsParser.Parse();
-            
-            //generate c# code
-            //TODO: add business actions descriptions to generated code
-            string generatedFileName = testCasesXML.FullName.Substring(0, testCasesXML.FullName.Length - 4);
-            string csharpCodeFile = generatedFileName + ".cs";
-            System.Console.WriteLine("Creating '{0}'", csharpCodeFile);
-            using (ICodeWriter writer = new FileCodeWriter(csharpCodeFile))
-            {
-                ITestCodeGenerator cSharpCode = new CSharpTestCodeGenerator(writer);
-                cSharpCode.Generate(testSpecs);
-            }
+			//validate XML content
+			Stream streamBusinessActions =
+				AccipioHelper.GetXmlFileContent(businessActionXML.FullName);
+			AccipioHelper.ValidateXmlDocument(streamBusinessActions, businessActionXSD.FullName);
 
-            //generate html test specifications
-            //TODO: add business actions descriptions to test spec
-            string htmlCodeFile = generatedFileName + ".html";
-            System.Console.WriteLine("Creating '{0}'", htmlCodeFile);
-            using (ICodeWriter writer = new FileCodeWriter(htmlCodeFile))
-            {
-                ITestCodeGenerator htmlTestSpecs = new HtmlTestCodeGenerator(writer);
-                htmlTestSpecs.Generate(testSpecs);
-            }
-        }
+			//parse test cases
+			Stream streamTestCases =
+				AccipioHelper.GetXmlFileContent(testCasesXML.FullName);
+			XmlTestSpecsParser testSpecsParser = new XmlTestSpecsParser(streamTestCases);
+			testSpecs = testSpecsParser.Parse();
+		}
 
-        public void Process()
-        {
-            //TODO: move from parser
-        }
-    }
+		public void Process()
+		{
+			//generate c# code
+			//TODO: add business actions descriptions to generated code
+			string generatedFileName = testCasesXML.FullName.Substring(0, testCasesXML.FullName.Length - 4);
+			string csharpCodeFile = generatedFileName + ".cs";
+			System.Console.WriteLine("Creating '{0}'", csharpCodeFile);
+			using (ICodeWriter writer = new FileCodeWriter(csharpCodeFile))
+			{
+				ITestCodeGenerator cSharpCode = new CSharpTestCodeGenerator(writer);
+				cSharpCode.Generate(testSpecs);
+			}
+
+			//generate html test specifications
+			//TODO: add business actions descriptions to test spec
+			string htmlCodeFile = generatedFileName + ".html";
+			System.Console.WriteLine("Creating '{0}'", htmlCodeFile);
+			using (ICodeWriter writer = new FileCodeWriter(htmlCodeFile))
+			{
+				ITestCodeGenerator htmlTestSpecs = new HtmlTestCodeGenerator(writer);
+				htmlTestSpecs.Generate(testSpecs);
+			}
+		}
+
+		private TestSpecs testSpecs;
+		//private BusinessActions businessActions;
+
+		private FileInfo businessActionXML;
+		private FileInfo businessActionXSD;
+		//TODO: possible more than one XML file with test cases.
+		private FileInfo testCasesXML;
+	}
 }
