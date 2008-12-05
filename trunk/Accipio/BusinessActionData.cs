539a45537fe497a20a2087a2af8c551bcf500ead
andrej.jeznik <andrej.jeznik@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-05 14:31:35 +0000

    Working on CSharpTestCodeGenerator.

--- trunk/Accipio/BusinessActionData.cs
+++ trunk/Accipio/BusinessActionData.cs
@@ -8,6 +8,24 @@ namespace Accipio
     public class BusinessActionData
     {
         /// <summary>
+        /// Gets the specified action.
+        /// </summary>
+        /// <param name="actionId">The action id.</param>
+        /// <returns>Requested action or <c>null</c> See <see cref="BusinessActionEntry"/></returns>
+        public BusinessActionEntry GetAction(string actionId)
+        {
+            foreach (BusinessActionEntry businessActionEntry in actions)
+            {
+                if (businessActionEntry.ActionId == actionId)
+                {
+                    return businessActionEntry;
+                }
+            }
+
+            return null;
+        }
+
+        /// <summary>
         /// Gets list of all business actions.
         /// </summary>
         public IList<BusinessActionEntry> Actions
