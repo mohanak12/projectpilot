1280b4c4250fb3e2020889c9d71edc5aebc8a87f
jure.hribar@gmail.com <jure.hribar@gmail.com@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-16 11:08:11 +0000

    Comments added.

--- trunk/ProjectPilot.Framework/Metrics/GroupLocMetricsBase.cs
+++ trunk/ProjectPilot.Framework/Metrics/GroupLocMetricsBase.cs
@@ -6,8 +6,16 @@ using Flubu.Builds.VSSolutionBrowsing;
 
 namespace ProjectPilot.Framework.Metrics
 {
+    /// <summary>
+    /// Abstract class derived from LocMetricsBase. 
+    /// Used to hold groups (nodes) of loc metrics items
+    /// </summary>
     public abstract class GroupLocMetricsBase : LocMetricsBase
     {
+        /// <summary>
+        /// Gets the loc stats data.
+        /// </summary>
+        /// <returns>Returns the combined loc stats of the group</returns>
         public override LocStatsData GetLocStatsData()
         {
             LocStatsData groupData = new LocStatsData();
@@ -21,11 +29,18 @@ namespace ProjectPilot.Framework.Metrics
             return groupData;
         }
 
+        /// <summary>
+        /// Adds another loc metrics item to teh group.
+        /// </summary>
+        /// <param name="locMetrics">Loc metrics item.</param>
         protected void AddLocMetrics(LocMetricsBase locMetrics)
         {
             groupLocStatsData.Add(locMetrics);
         }
 
+        /// <summary>
+        /// The list of loc metrics items.
+        /// </summary>
         private List<LocMetricsBase> groupLocStatsData = new List<LocMetricsBase>();
     }
 }
