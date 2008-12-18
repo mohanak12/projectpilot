d0e102b4d454910dd597c1f7ef38a29d9130b605
jure.hribar@gmail.com <jure.hribar@gmail.com@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-18 10:10:28 +0000

    Remove & sort using.

--- trunk/ProjectPilot.Framework/Metrics/VSSolutionLocMetrics.cs
+++ trunk/ProjectPilot.Framework/Metrics/VSSolutionLocMetrics.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Flubu.Builds.VSSolutionBrowsing;
+﻿using Flubu.Builds.VSSolutionBrowsing;
 
 namespace ProjectPilot.Framework.Metrics
 {
