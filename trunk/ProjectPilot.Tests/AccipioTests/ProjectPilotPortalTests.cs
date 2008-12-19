ad8d2bb04eed529bbf7f45b1edded940e129ce03
andrej.jeznik <andrej.jeznik@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-19 08:40:35 +0000

    Added usings to generated csharp code.

--- trunk/ProjectPilot.Tests/AccipioTests/ProjectPilotPortalTests.cs
+++ trunk/ProjectPilot.Tests/AccipioTests/ProjectPilotPortalTests.cs
@@ -11,15 +11,20 @@ namespace ProjectPilot.Tests.AccipioTests
         /// </summary>
         /// <remarks>It is used as example for csharp code generator.</remarks>
         [Test, Pending("Not ready yet")]
-        public void GoToPortal()
+        public void ClickSearchButton()
         {
             using (ProjectPortalTestRunner runner = new ProjectPortalTestRunner())
             {
                 runner
                     .SetDescription("descr")
                     .AddTag("tag1")
+                    
                     .ProjectPortal
+                    //description of action GoToPortal
+                    .GoToPortal("/Default.aspx")
+                    //description of action FindButton
                     .FindButton("Search")
+                    //description of action ClickOnButton
                     .ClickOnButton("Search");
             }
         }
