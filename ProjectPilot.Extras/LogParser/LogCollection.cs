1614c8aa97cab0d20dc2f95673276e9adf5648fe
jure.hribar@gmail.com <jure.hribar@gmail.com@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-23 08:51:03 +0000

    Log parser class.

---
+++ ProjectPilot.Extras/LogParser/LogCollection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace ProjectPilot.Extras.LogParser
+{
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses")]
+    class LogCollection
+    {
+    }
+}
