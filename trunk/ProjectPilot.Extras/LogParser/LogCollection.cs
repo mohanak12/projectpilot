37f54f6d6aad3385d92719974ed8b77bc8be78f9
jure.hribar@gmail.com <jure.hribar@gmail.com@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-23 09:54:28 +0000

    LogCollection work in progress.

--- trunk/ProjectPilot.Extras/LogParser/LogCollection.cs
+++ trunk/ProjectPilot.Extras/LogParser/LogCollection.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 using System.Linq;
 using System.Text;
 
 namespace ProjectPilot.Extras.LogParser
 {
     [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses")]
-    class LogCollection
+    [SuppressMessage("Microsoft.Naming", "CA1711:IdentifiersShouldNotHaveIncorrectSuffix")]
+    public class LogCollection
     {
+        public void ParseLogFile(Stream fileStream)
+        {
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null) //End of file.
+                        break;
+
+                    //LogEntry newEntry = new LogEntry();
+
+                    //string [] entryArray = line.Split(' ');
+
+                    //newEntry.Date = line.Substring(0, line.IndexOf(@" ");
+                }
+            }
+        }
     }
 }
