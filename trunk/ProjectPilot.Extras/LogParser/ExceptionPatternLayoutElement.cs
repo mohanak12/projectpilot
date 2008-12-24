0beec8fbead21f823c8a5847a8ee41735447d57c
jure.hribar@gmail.com <jure.hribar@gmail.com@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-24 12:17:55 +0000

    Pattern classes added for logging.

---
+++ trunk/ProjectPilot.Extras/LogParser/ExceptionPatternLayoutElement.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ProjectPilot.Extras.LogParser
+{
+    [SuppressMessage("Microsoft.Performance", "CA1812:AvoidUninstantiatedInternalClasses")]
+    class ExceptionPatternLayoutElement : ILog4NetPatternLayoutElement
+    {
+        public int Parse(string line, int startingIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
