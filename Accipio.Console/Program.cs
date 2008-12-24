9d2348d76ec7733a30f60835f5cbd55a925fbfc4
gregor.kocevar <gregor.kocevar@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-24 08:32:20 +0000

    Added tests for ConsoleApp class

--- Accipio.Console/Program.cs
+++ Accipio.Console/Program.cs
@@ -2,10 +2,10 @@
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static int Main(string[] args)
         {
             ConsoleApp consoleApp = new ConsoleApp(args);
-            consoleApp.Process();
+            return consoleApp.Process();
         }
     }
 }
