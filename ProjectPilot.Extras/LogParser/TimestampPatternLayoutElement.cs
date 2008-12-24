b4824640b67ff6fa4379d067cbaf31caab54e598
ockom8 <ockom8@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-24 09:45:07 +0000

    Added missing files.

---
+++ ProjectPilot.Extras/LogParser/TimestampPatternLayoutElement.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ProjectPilot.Extras.LogParser
+{
+    public class TimestampPatternLayoutElement : ILog4NetPatternLayoutElement
+    {
+        public int Parse(string line, int startingIndex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
