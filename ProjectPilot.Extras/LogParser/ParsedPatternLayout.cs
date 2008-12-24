021eb88538e6c911b3677f36a0d90014b9b0d706
ockom8 <ockom8@5ada41ee-a197-11dd-9b86-41075523de00>
2008-12-24 13:55:06 +0000

    Working on new LogParser

--- ProjectPilot.Extras/LogParser/ParsedPatternLayout.cs
+++ ProjectPilot.Extras/LogParser/ParsedPatternLayout.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 
 namespace ProjectPilot.Extras.LogParser
 {
@@ -8,10 +10,55 @@ namespace ProjectPilot.Extras.LogParser
         // %timestamp [%thread] %level %logger %ndc - %message%newline
         // PatternPatternLayoutElement (PatternPatternLayoutElementType.TimeStamp)
         // LiteralPatternLayoutElement (" [")
-        // ....
+        // ....\
 
+        public void ParsePattern(string line)
+        {
+//            int index, 
+              int count = 0;
+              string patternElement = "", literal = "";
+//            index = line.IndexOf(' ');
+           //element = line.Substring(line.IndexOf('%'), line.IndexOf(' '));
+
+            bool test = line.Contains("timestamp");
+
+            Regex regex= new Regex(@"%-?[0-9]*\.?[0-9]*(?<pattern>[a-zA-Z]+)");
+           //Regex regex1 = new Regex(@"%-(?<min>[0-9]*)\.(?<max>[0-9]*)(?<pattern>[a-zA-Z]+)");
+
+            string[] parseArray = line.Split(' ');
+
+            foreach (string pattern in parseArray)
+            {
+                if(regex.IsMatch(pattern))
+                {
+                    Match match = regex.Match(pattern);
+                    count++;
+                }
+            }
+
+
+            for (int n=0; n<line.Length; n++)
+            {
+                    if(line[n] == '%')
+                    {
+                      patternElement = line.Substring(line.IndexOf('%', n), line.IndexOf(' '));
+                      n = n + patternElement.Length;
+                    }
+                    if(line[n] != '%')
+                    {
+                        literal = literal + line[n];
+                    }
+            }
+        }
+
+        //if(patternElement)
 
         [SuppressMessage("Microsoft.Performance", "CA1823:AvoidUnusedPrivateFields")]
         private List<ILog4NetPatternLayoutElement> elements = new List<ILog4NetPatternLayoutElement>();
+
+        //private string line = @"%timestamp [%thread] %level %logger %ndc - %message%newline";
+//        private string line1 = @"%-6timestamp [%15.15thread] %-5level %30.30logger %ndc - %message%newline";
+//        private string patternElement = "%-6timestamp";
+//        private string patternElement1 = "";
     }
 }
--- ProjectPilot.Tests/Extras/LogParserTest.cs
+++ ProjectPilot.Tests/Extras/LogParserTest.cs
@@ -13,20 +13,24 @@ namespace ProjectPilot.Tests.Extras
         [Test, Pending]
         public void TestLogParser()
         {
-            Regex regexDate = new Regex(@"^\d{4}-\d{2}-\d{2}$");
-            Regex regexThreadId = new Regex(@"\[\d+\]");
-            //PatternPatternLayoutElementTypeRegex regexLevel = new Regex(@"\[\d+\]");
-            Regex regexTime = new Regex(@"^\d{2}:\d{2}:\d{2},\d{3}$");
+//            Regex regexDate = new Regex(@"^\d{4}-\d{2}-\d{2}$");
+//            Regex regexThreadId = new Regex(@"\[\d+\]");
+//            //PatternPatternLayoutElementTypeRegex regexLevel = new Regex(@"\[\d+\]");
+//            Regex regexTime = new Regex(@"^\d{2}:\d{2}:\d{2},\d{3}$");
+//
+//            Dictionary<string, Regex> pattern = new Dictionary<string, Regex>();
+//            pattern.Add("Date", regexDate);
+//            pattern.Add("Time", regexTime);
+//            pattern.Add("ThreadId", regexThreadId);
+//            
+//            Stream stream = File.OpenRead(@"..\..\..\Data\Samples\TestServer.log");
+//            LogCollection parseCollection = new LogCollection();
+//
+//            parseCollection.ParseLogFile(stream, pattern);
 
-            Dictionary<string, Regex> pattern = new Dictionary<string, Regex>();
-            pattern.Add("Date", regexDate);
-            pattern.Add("Time", regexTime);
-            pattern.Add("ThreadId", regexThreadId);
-            
-            Stream stream = File.OpenRead(@"..\..\..\Data\Samples\TestServer.log");
-            LogCollection parseCollection = new LogCollection();
-
-            parseCollection.ParseLogFile(stream, pattern);
+            ParsedPatternLayout patternParse = new ParsedPatternLayout();
+              patternParse.ParsePattern("%timestamp [%thread] %level %logger %ndc - %message%newline");
+           // patternParse.ParsePattern("%-6timestamp");
         }
     }
 }
