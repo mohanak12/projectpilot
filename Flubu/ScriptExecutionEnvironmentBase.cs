6b5b98fbe6170a9299d5fc09ffff7a8780b9cbbf
igor.brejc <igor.brejc@5ada41ee-a197-11dd-9b86-41075523de00>
2009-01-05 13:28:56 +0000

    Added InteractiveMode to the IScriptExecutionEnvironment.

--- Flubu/ScriptExecutionEnvironmentBase.cs
+++ Flubu/ScriptExecutionEnvironmentBase.cs
@@ -17,6 +17,19 @@ namespace Flubu
         }
 
         /// <summary>
+        /// Gets or sets a value indicating whether the execution environment runs in an interactive mode.
+        /// </summary>
+        /// <remarks>When the execution environment is running in the interactive mode, the user has
+        /// the ability to influence certain aspects of the environment. One example is entering values
+        /// for missing configuration settings.</remarks>
+        /// <value><c>true</c> if the execution environment runs in an interactive mode; otherwise, <c>false</c>.</value>
+        public bool InteractiveMode
+        {
+            get { return interactiveMode; }
+            set { interactiveMode = value; }
+        }
+
+        /// <summary>
         /// Gets a value indicating whether the script is running on Windows Server 2003.
         /// </summary>
         /// <value>
@@ -198,9 +211,8 @@ namespace Flubu
 
         private bool disposed;
         private bool dryRun;
-        
-        private readonly List<IFlubuLogger> loggers = new List<IFlubuLogger>();
-        
+        private bool interactiveMode;
+        private readonly List<IFlubuLogger> loggers = new List<IFlubuLogger>();        
         private string scriptName;
     }
 }
