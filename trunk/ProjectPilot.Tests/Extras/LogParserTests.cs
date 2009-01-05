3c77135b8d3e90d438665340b63bb8ddb04a48a6
ockom8 <ockom8@5ada41ee-a197-11dd-9b86-41075523de00>
2009-01-05 14:35:41 +0000

    LogParser - Added support for different time formats.

--- trunk/ProjectPilot.Tests/Extras/LogParserTests.cs
+++ trunk/ProjectPilot.Tests/Extras/LogParserTests.cs
@@ -30,11 +30,11 @@ namespace ProjectPilot.Tests.Extras
             Assert.AreEqual(typeof(TimestampElement).FullName, lineParse.ElementsLog[0].Elements[0].GetType().FullName);
             Assert.AreEqual(typeof(ThreadIdElement).FullName, lineParse.ElementsLog[0].Elements[1].GetType().FullName);
 
-            DateTime expectedTime = DateTime.ParseExact("2008-12-22 09:16:02,734", "yyyy-MM-dd HH:mm:ss,fff", CultureInfo.CurrentCulture);
-            
+            DateTime expectedTime = DateTime.ParseExact("2008-12-22 09:16:02,734", "yyyy-MM-dd HH:mm:ss,fff", CultureInfo.CurrentCulture);  
             Assert.AreEqual(
                 expectedTime, 
                 ((ParsedElementBase)lineParse.ElementsLog[0].Elements[0]).Element);
+           
             Assert.AreEqual(
                 "[4904]",
                 ((ParsedElementBase)lineParse.ElementsLog[0].Elements[1]).Element);
@@ -116,6 +116,40 @@ namespace ProjectPilot.Tests.Extras
 
                 Assert.AreEqual(15, lineParse.ElementsLog.Count);
             }
+        }
+
+        [Test]
+        public void ParsingDifferentTimePatterns()
+        {
+            string timePattern = "yyyy-MM-dd HH:mm:ss.fff";
+            LogCollection lineParse = new LogCollection('|', "Time|ThreadId", timePattern);
+
+            lineParse.ParseLogLine(@"2008-12-22 09:16:02.734|[4904]");
+
+            DateTime expectedTime = DateTime.ParseExact("2008-12-22 09:16:02.734", timePattern, CultureInfo.CurrentCulture);
+            Assert.AreEqual(
+                expectedTime,
+                ((ParsedElementBase)lineParse.ElementsLog[0].Elements[0]).Element);
+
+            timePattern = "MM+dd+yy HH:mm:ss,fff";
+            LogCollection lineParse2 = new LogCollection('|', "Time|ThreadId", timePattern);
+
+            lineParse2.ParseLogLine(@"12+22+08 09:16:02,734|[4904]");
+
+            expectedTime = DateTime.ParseExact("12+22+08 09:16:02,734", timePattern, CultureInfo.CurrentCulture);
+            Assert.AreEqual(
+                expectedTime,
+                ((ParsedElementBase)lineParse2.ElementsLog[0].Elements[0]).Element);
+
+            timePattern = "MMddyy";
+            LogCollection lineParse3 = new LogCollection('|', "Time|ThreadId", timePattern);
+
+            lineParse3.ParseLogLine(@"122208|[4904]");
+
+            expectedTime = DateTime.ParseExact("122208", timePattern, CultureInfo.CurrentCulture);
+            Assert.AreEqual(
+                expectedTime,
+                ((ParsedElementBase)lineParse3.ElementsLog[0].Elements[0]).Element);
         }
 
         [FixtureSetUp]
