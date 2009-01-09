b7ac13d74ebdd3492d542d4ab0679753e681859b
andrej.jeznik <andrej.jeznik@5ada41ee-a197-11dd-9b86-41075523de00>
2009-01-09 15:07:46 +0000

    Started with xslt Transform.

---
+++ trunk/ProjectPilot.Tests/AccipioTests/TestReportGeneratorCommandTests.cs
@@ -0,0 +1,24 @@
+﻿using Accipio.Console;
+using MbUnit.Framework;
+
+namespace ProjectPilot.Tests.AccipioTests
+{
+    [TestFixture]
+    public class TestReportGeneratorCommandTests
+    {
+        [Test, Pending("Not finished yet")]
+        public void Transform()
+        {
+            string[] args = new string[]
+                                {
+                                    "transform",
+                                    "..\\..\\..\\Data\\Samples\\TestResults-0.xml",
+                                    "TestResults.xml"
+                                };
+            TestReportGeneratorCommand testReportGeneratorCommand = new TestReportGeneratorCommand(null);
+            testReportGeneratorCommand.AccipioDirectory = "..\\..\\..\\Accipio.Console";
+            testReportGeneratorCommand.ParseArguments(args);
+            testReportGeneratorCommand.ProcessCommand();
+        }
+    }
+}
