09d2826fa95fc4d54d6126237bcd4227567da6b4
gregor.kocevar <gregor.kocevar@5ada41ee-a197-11dd-9b86-41075523de00>
2009-01-20 15:54:32 +0000

    Added HtmlTestReportGenerator

---
+++ trunk/Accipio/UserStory.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Collections;
+using System.Globalization;
+
+namespace Accipio
+{
+    public class UserStory
+    {
+        public UserStory(string userStoryName)
+        {
+            this.userStoryName = userStoryName;
+        }
+
+        public int PresenceInTestCase { get; set; }
+
+        public int SuccessfullyAccomplished { get; set; }
+
+        public string UserStoryName
+        {
+            get { return userStoryName; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format(
+                CultureInfo.InvariantCulture, 
+                "{0} ({1}/{2})", 
+                userStoryName, 
+                SuccessfullyAccomplished,
+                PresenceInTestCase);
+        }
+
+        private readonly string userStoryName;
+    }
+}
