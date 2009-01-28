0eebbdf309848b7600692fc9c506000d4076f42e
igor.brejc <igor.brejc@5ada41ee-a197-11dd-9b86-41075523de00>
2009-01-28 07:42:27 +0000

    Continued refactoring Accipio Reporting.

--- Accipio/Reporting/TestCaseRun.cs
+++ Accipio/Reporting/TestCaseRun.cs
@@ -1,7 +1,39 @@
+using System.Collections.Generic;
+
 namespace Accipio.Reporting
 {
     public class TestCaseRun
     {
+        public TestCaseRun(string testCaseId, TestCaseExecutionStatus status)
+        {
+            this.testCaseId = testCaseId;
+            this.status = status;
+        }
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public TestCaseExecutionStatus Status
+        {
+            get { return status; }
+        }
+
+        public string TestCaseId
+        {
+            get { return testCaseId; }
+        }
+
+        public void AddUserStory (string userStoryId)
+        {
+            userStories.Add(userStoryId, null);
+        }
+
         private string testCaseId;
+        private TestCaseExecutionStatus status;
+        private string message;
+        private Dictionary<string, string> userStories = new Dictionary<string, string>();
     }
 }
--- Accipio/Reporting/TestRun.cs
+++ Accipio/Reporting/TestRun.cs
@@ -7,8 +7,32 @@ namespace Accipio.Reporting
 {
     public class TestRun
     {
+        public void AddTestSuiteRun (TestSuiteRun testSuiteRun)
+        {
+            testSuitesRuns.Add(testSuiteRun.TestSuiteId, testSuiteRun);
+        }
+
+        public DateTime EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+
+        public DateTime StartTime
+        {
+            get { return startTime; }
+            set { startTime = value; }
+        }
+
+        public Version Version
+        {
+            get { return version; }
+            set { version = value; }
+        }
+
         private DateTime endTime;
         private DateTime startTime;
         private Dictionary<string, TestSuiteRun> testSuitesRuns = new Dictionary<string, TestSuiteRun>();
+        private Version version;
     }
 }
