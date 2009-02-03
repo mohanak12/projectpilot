54f4cb2caf268389b96f5ce2065749ee11817854
ockom8 <ockom8@5ada41ee-a197-11dd-9b86-41075523de00>
2009-02-03 14:54:32 +0000

    Log4NetBrowser - Support for direct static URL added (LogView).

---
+++ trunk/ProjectPilot.Log4NetBrowser/Models/ParseLogFile.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml;
+
+namespace ProjectPilot.Log4NetBrowser.Models
+{
+    public class ParseLogFile
+    {
+        public static LogDisplay ParseFile(string logKey)
+        {
+            LogDisplay parserContent = new LogDisplay();
+
+            Dictionary<string, string> logFiles = new Dictionary<string, string>();
+            Dictionary<string, string> logFilesPatterns = new Dictionary<string, string>();
+            Dictionary<string, char> logFilesSeparators = new Dictionary<string, char>();
+
+            XmlReaderSettings xmlReaderSettings = new XmlReaderSettings();
+            xmlReaderSettings.IgnoreComments = true;
+            xmlReaderSettings.IgnoreProcessingInstructions = true;
+            xmlReaderSettings.IgnoreWhitespace = true;
+
+            using (XmlReader xmlReader = XmlReader.Create(System.IO.File.OpenRead(@"\\zarja\share\Marko\LogConfig.xml"), xmlReaderSettings))
+            {
+                string key;
+
+                while (false == xmlReader.EOF)
+                {
+                    xmlReader.Read();
+
+                    if (xmlReader.Name == "LogURL" && xmlReader.NodeType != XmlNodeType.EndElement)
+                    {
+                        key = xmlReader["logKey"];
+
+                        logFilesPatterns.Add(key, xmlReader["logPattern"]);
+                        logFilesSeparators.Add(key, char.Parse(xmlReader["separator"]));
+
+                        xmlReader.Read();
+
+                        logFiles.Add(key, xmlReader.Value);
+                    }
+                }
+            }
+
+            string fileSelected = string.Empty;
+            string filePattern = string.Empty;
+            char fileSeparator = '|'; //Default separator
+
+            if (logFiles.ContainsKey(logKey))
+            {
+                fileSelected = logFiles[logKey];
+                filePattern = logFilesPatterns[logKey];
+                fileSeparator = logFilesSeparators[logKey];
+                
+                parserContent.Parsing10MBLogFile(null, fileSelected, filePattern, fileSeparator);
+            }
+            else
+            {
+                parserContent.Parsing10MBLogFile(null, null, null, null);
+            }
+            
+            return parserContent;
+        }
+    }
+}
