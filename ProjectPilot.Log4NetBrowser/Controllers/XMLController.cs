58ae555ca07fb9c5df69ea2c641e8ed5667e4627
ockom8 <ockom8@5ada41ee-a197-11dd-9b86-41075523de00>
2009-02-16 07:42:45 +0000

    Added missing files.

---
+++ ProjectPilot.Log4NetBrowser/Controllers/XMLController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml;
+using System.Xml.Linq;
+using ProjectPilot.Framework.Metrics;
+
+namespace ProjectPilot.Log4NetBrowser.Controllers
+{
+    public class XMLController : Controller
+    {
+        public ActionResult SelectSolutionFile()
+        {
+            return View();
+
+
+        }
+
+        public ActionResult GenerateXML(string solutionFilePath)
+        {
+            VSSolutionLocMetrics metrics =
+                new VSSolutionLocMetrics(solutionFilePath.Substring(solutionFilePath.LastIndexOf(@"\") + 1));
+
+            metrics.LocStatsMap.AddToMap(".cs", new CSharpLocStats());
+            metrics.CalculateLocForSolution(solutionFilePath);
+            metrics.GenerateXmlReport(@"XML_report.xml");
+
+            return RedirectToAction("XML", "XML");
+        }
+
+        public ActionResult XML()
+        {
+            return View();
+        }
+    }
+}
