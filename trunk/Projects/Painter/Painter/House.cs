57706c0c6699276a884a6a053bf6682e17753b6d
ockom8 <ockom8@5ada41ee-a197-11dd-9b86-41075523de00>
2009-04-03 14:00:28 +0000

    Task 2: Painter, Stage 5

---
+++ trunk/Projects/Painter/Painter/House.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Painter
+{
+    public class House : PaintElement
+    {
+        public House(Point point, int length, int width) : base(point)
+        {
+            this.AddParameter(length);
+            this.AddParameter(width);
+        }
+
+        public override void Draw(IDrawingEngine engine)
+        {
+            int x = this.Point.X;
+            int y = this.Point.Y;
+            int height = this.Parameters[0];
+            int width = this.Parameters[1];
+
+            engine.Line(new Point(x, y+(y/8)), new Point(x + width/2, y), this.DrawingProperties);
+            engine.Line(new Point(x + width / 2, y), new Point(x + width, y + y/8), this.DrawingProperties);
+
+            Rectangle house = new Rectangle(new Point(x,  y+(y/8)), width, height);
+            house.Draw(engine);
+
+            Window window = new Window(new Point(x + x/5, y + y/5), height/5, width/5);
+            window.Draw(engine);
+
+            Window window1 = new Window(new Point(x - x / 5 + width, y + y / 5), height / 5, width / 5);
+            window1.Draw(engine);
+        }
+    }
+}
---
+++ trunk/Projects/Painter/Painter/Window.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Painter
+{
+    public class Window : PaintElement
+    {
+        public Window(Point point, int length, int width)
+            : base(point)
+        {
+            this.AddParameter(length);
+            this.AddParameter(width);
+        }
+
+        public override void Draw(IDrawingEngine engine)
+        {
+            Rectangle frame = new Rectangle(this.Point, this.Parameters[0], this.Parameters[1]);
+            frame.Draw(engine);
+
+            int x = this.Point.X;
+            int y = this.Point.Y;
+            int height = this.Parameters[0];
+            int width = this.Parameters[1];
+
+            engine.Line(new Point(x, y + width / 2), new Point(x + height, y + width / 2), this.DrawingProperties);
+            engine.Line(new Point(x + height / 2 ,y), new Point(x + height / 2, y + width), this.DrawingProperties);
+        }
+
+    }
+}
