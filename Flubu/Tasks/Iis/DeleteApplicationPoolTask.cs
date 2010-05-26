fbcec5472970c296d90fed84d42dff300ba39a52
zoro.zorec <zoro.zorec@5ada41ee-a197-11dd-9b86-41075523de00>
2010-05-26 08:10:54 +0000

    added new flubu Tasks   - DeleteApplicationPoolTask   - DeleteMessageQueueTask   - RemoveWindowsServiceTask

---
+++ Flubu/Tasks/Iis/DeleteApplicationPoolTask.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.DirectoryServices;
+using System.Globalization;
+using System.IO;
+
+namespace Flubu.Tasks.Iis
+{
+    public class DeleteApplicationPoolTask : TaskBase
+    {
+        public DeleteApplicationPoolTask(string applicationPoolName, bool failIfNotExist)
+        {
+            ApplicationPoolName = applicationPoolName;
+            FailIfNotExist = failIfNotExist;
+        }
+
+        public override string TaskDescription
+        {
+            get
+            {
+                return string.Format(
+                    CultureInfo.InvariantCulture,
+                    "Delete application pool '{0}'.",
+                    ApplicationPoolName);
+            }
+        }
+
+        public static void Execute(
+            IScriptExecutionEnvironment environment,
+            string applicationPoolName,
+            bool failIfNotExist)
+        {
+            DeleteApplicationPoolTask task = new DeleteApplicationPoolTask(applicationPoolName, failIfNotExist);
+            task.Execute(environment);
+        }
+
+        protected override void DoExecute(IScriptExecutionEnvironment environment)
+        {
+            string version = GetLocalIisVersionTask.GetIisVersion(environment, false);
+            int major = GetLocalIisVersionTask.GetMajorVersion(version);
+            if (major < 6 || major == 0)
+            {
+                environment.LogMessage("IIS does not support application pools.");
+                return;
+            }
+
+            const string AppPoolsRootName = @"IIS://localhost/W3SVC/AppPools";
+
+            using (DirectoryEntry parent = new DirectoryEntry(AppPoolsRootName))
+            {
+                DirectoryEntry applicationPoolEntry = null;
+
+                try
+                {
+                    try
+                    {
+                        applicationPoolEntry = parent.Children.Find(ApplicationPoolName, "IIsApplicationPool");
+                        applicationPoolEntry.DeleteTree();
+                        applicationPoolEntry.CommitChanges();
+                    }
+                    catch (DirectoryNotFoundException)
+                    {
+                        // application pool already exists
+                        if (FailIfNotExist)
+                        {
+                            throw new RunnerFailedException(
+                                String.Format(
+                                    CultureInfo.InvariantCulture,
+                                    "Application '{0}' does not exist.",
+                                    ApplicationPoolName));
+                        }
+
+                        environment.LogMessage(
+                            String.Format(
+                                CultureInfo.InvariantCulture,
+                                "Application pool '{0}' does not exist, doing nothing.",
+                                ApplicationPoolName));
+                        return;
+                    }
+                }
+                finally
+                {
+                    if (applicationPoolEntry != null)
+                        applicationPoolEntry.Dispose();
+                }
+
+                parent.CommitChanges();
+            }
+        }
+
+        private string ApplicationPoolName { get; set; }
+
+        private bool FailIfNotExist { get; set; }
+    }
+}
---
+++ Flubu/Tasks/WindowsServices/RemoveWindowsServiceTask.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+
+namespace Flubu.Tasks.WindowsServices
+{
+    public class RemoveWindowsServiceTask : TaskBase
+    {
+        public override string TaskDescription
+        {
+            get
+            {
+                return String.Format(
+                    CultureInfo.InvariantCulture,
+                    "Uninstall Windows service '{0}'",
+                    ServiceName);
+            }
+        }
+
+        public RemoveWindowsServiceTask(string serviceName, bool failIfNotExist)
+        {
+            ServiceName = serviceName;
+            FailIfNotExist = failIfNotExist;
+        }
+
+        public static void Execute(
+            IScriptExecutionEnvironment environment,
+            string serviceName,
+            bool failIfNotExist)
+        {
+            RemoveWindowsServiceTask task = new RemoveWindowsServiceTask(serviceName, failIfNotExist);
+            task.DoExecute(environment);
+        }
+
+        protected override void DoExecute(IScriptExecutionEnvironment environment)
+        {
+// ReSharper disable InconsistentNaming
+            const int GenericWrite = 0x40000000;
+// ReSharper restore InconsistentNaming
+            IntPtr scHndl = IntPtr.Zero;
+            IntPtr svcHndl = IntPtr.Zero;
+            try
+            {
+                scHndl = OpenSCManager(null, null, GenericWrite);
+                if (scHndl == IntPtr.Zero)
+                {
+                    throw new RunnerFailedException("Service manager could not be opened!");
+                }
+
+// ReSharper disable InconsistentNaming
+                const int Delete = 0x10000;
+// ReSharper restore InconsistentNaming
+                svcHndl = OpenService(scHndl, ServiceName, Delete);
+                if (svcHndl == IntPtr.Zero)
+                {
+                    if (FailIfNotExist)
+                        throw new RunnerFailedException("Service {0} does not exist.", ServiceName);
+
+                    environment.LogMessage("Service '{0}' does not exist, doing nothing.", ServiceName);
+                    return;
+                }
+
+                int result = DeleteService(svcHndl);
+                if (result != 0)
+                {
+                    throw new RunnerFailedException("Uninstall windows service failed with error code:{0}", result);
+                }
+            }
+            finally
+            {
+                if (svcHndl != IntPtr.Zero)
+                    CloseServiceHandle(svcHndl);
+                if (scHndl != IntPtr.Zero)
+                    CloseServiceHandle(scHndl);
+            }
+        }
+
+        #region DLLImport
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
+        [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass")]
+        // ReSharper disable InconsistentNaming
+        private static extern IntPtr OpenSCManager(string lpMachineName, string lpScdb, int scParameter);
+        // ReSharper restore InconsistentNaming
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
+        [SuppressMessage("Microsoft.Portability", "CA1901:PInvokeDeclarationsShouldBePortable", MessageId = "return")]
+        [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass")]
+        private static extern void CloseServiceHandle(IntPtr schandle);
+
+        [DllImport("advapi32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+        [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass")]
+        private static extern IntPtr OpenService(IntPtr schandle, string lpSvcName, int dwNumServiceArgs);
+
+        [DllImport("advapi32.dll", CharSet = CharSet.Unicode)]
+        [SuppressMessage("Microsoft.Design", "CA1060:MovePInvokesToNativeMethodsClass")]
+        private static extern int DeleteService(IntPtr svhandle);
+
+        #endregion DLLImport
+
+        private string ServiceName { get; set; }
+
+        private bool FailIfNotExist { get; set; }
+    }
+}
