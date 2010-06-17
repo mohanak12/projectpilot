4f176158bc01ca9196a5d4decb8825286148b843
zoro.zorec <zoro.zorec@5ada41ee-a197-11dd-9b86-41075523de00>
2010-06-17 08:17:33 +0000

    - control windows service on another Machine - base runner (command line parsing and task execution)

---
+++ trunk/Flubu/RunnerBase.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Flubu.Builds;
+
+namespace Flubu
+{
+    public abstract class RunnerBase<TRunner> : BuildRunner<TRunner>
+        where TRunner : BuildRunner<TRunner>
+    {
+        protected RunnerBase(string productId, string[] args) : base(productId)
+        {
+            Arguments = args;
+        }
+
+        public string[] Arguments { get; set; }
+
+        public abstract void Configure();
+
+        public virtual int Run()
+        {
+            try
+            {
+                // actual run
+                if (Arguments.Length == 0)
+                {
+                    RunTarget(DefaultTarget.TargetName);
+                    Complete();
+                    return 0;
+                }
+
+                foreach (string argument in Arguments)
+                {
+                    if (HasTarget(argument)) continue;
+
+                    ScriptExecutionEnvironment.LogError(
+                        "ERROR: The target '{0}' does not exist",
+                        argument);
+                    RunTarget("help");
+                    return 2;
+                }
+
+                foreach (string argument in Arguments)
+                {
+                    RunTarget(argument);
+                }
+
+                Complete();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
+        }
+    }
+}
