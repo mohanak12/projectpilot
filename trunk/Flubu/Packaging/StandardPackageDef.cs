7f57518a22d94f94e01f2c85ff6f3880c7f83a23
zoro.zorec <zoro.zorec@5ada41ee-a197-11dd-9b86-41075523de00>
2010-07-02 07:43:32 +0000

    simplified Flubu Copier usage

--- trunk/Flubu/Packaging/StandardPackageDef.cs
+++ trunk/Flubu/Packaging/StandardPackageDef.cs
@@ -9,5 +9,39 @@ namespace Flubu.Packaging
         public StandardPackageDef(string id) : base(id)
         {
         }
+
+        public StandardPackageDef(string id, ILogger logger, IDirectoryFilesLister directoryFilesLister) : base(id)
+        {
+            Logger = logger;
+            FileLister = directoryFilesLister;
+        }
+
+        public StandardPackageDef AddFolderSource(string id, string directoryName, bool recursive)
+        {
+            DirectorySource source = new DirectorySource(Logger, FileLister, id, directoryName, recursive);
+            AddFilesSource(source);
+            return this;
+        }
+
+        public StandardPackageDef AddFolderSource(string id, string directoryName, bool recursive, IFileFilter filter)
+        {
+            DirectorySource source = new DirectorySource(Logger, FileLister, id, directoryName, recursive);
+            source.SetFilter(filter);
+            AddFilesSource(source);
+            return this;
+        }
+
+        public StandardPackageDef AddWebFolderSource(string id, string directoryName, bool recursive)
+        {
+            DirectorySource source = new DirectorySource(Logger, FileLister, id, directoryName, recursive);
+            source.SetFilter(new NegativeFilter(
+                    new RegexFileFilter(@"^.*\.(svc|asax|config|aspx|ascx|css|js|gif|PNG)$")));
+            AddFilesSource(source);
+            return this;
+        }
+
+        private ILogger Logger { get; set; }
+
+        private IDirectoryFilesLister FileLister { get; set; }
     }
 }
